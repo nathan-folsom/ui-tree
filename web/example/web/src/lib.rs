e3ac78705825807d2d737c701caaaa44bad097d1
Nathan Folsom <n8folsom@gmail.com>
2022-09-18 16:07:31 -0400

    basic counter setup for showing concept in web example

--- web/example/web/src/lib.rs
+++ web/example/web/src/lib.rs
@@ -1,11 +1,16 @@
 mod utils;
 
-use heartwood::provider::Scope;
+use heartwood::{
+    provider::{ProviderTree, Scope},
+    root::RootNode,
+};
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1;
 
 #[wasm_bindgen]
 pub fn init_tree() {
+    let provider_tree = ProviderTree::new(None);
+    let counter_root = RootNode::new(&|| 0, &provider_tree);
     const SCOPEY_DOPE: Scope = Scope::new("Web Example Scope");
     let dbg = JsValue::from_str(&format!("{SCOPEY_DOPE:?}"));
     log_1(&dbg);
--- web/example/www/index.js
+++ web/example/www/index.js
@@ -1,3 +1,18 @@
 import * as wasm from "example-tree";
 
 wasm.init_tree();
+
+const counterContainer = document.getElementById("counter");
+const counter = document.createElement("div");
+
+const counterTitle = document.createElement("h3");
+counterTitle.innerText = "Count";
+
+const counterButton = document.createElement("button");
+counterButton.onclick = _ => console.log("clicked");
+counterButton.innerText = "Click me";
+
+counter.appendChild(counterTitle);
+counter.appendChild(counterButton);
+
+counterContainer.appendChild(counter);
