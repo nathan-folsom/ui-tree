9e8998a7a76c762c6a483f702b07d623cda1f57e
Nathan Folsom <n8folsom@gmail.com>
2022-09-18 13:59:02 -0400

    removing unused wee-alloc

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -4,12 +4,6 @@ use heartwood::provider::Scope;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn init_tree() {
     const SCOPEY_DOPE: Scope = Scope::new("Web Example Scope");
