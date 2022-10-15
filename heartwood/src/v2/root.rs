580ab620a4f521e90d884e89eb4cdd9cb0ef8ce5
Nathan Folsom <n8folsom@gmail.com>
2022-10-14 20:45:50 -0700

    bring root node logic into v2

--- heartwood/src/v2/root.rs
+++ heartwood/src/v2/root.rs
@@ -1,5 +1,9 @@
+use std::fmt::Debug;
+use std::rc::Rc;
+use crate::common::{Read, Write};
+use crate::v2::accessor::Accessible;
 use crate::v2::data_provider::DataProvider;
-use crate::v2::provider_tree::ProviderTree;
+use crate::v2::provider_tree::{Provided, ProviderTree, Scope};
 
 pub struct RootNode<T: 'static> {
     provider: DataProvider<T>,
@@ -14,5 +18,81 @@ impl<T: 'static> RootNode<T> {
             provider: DataProvider::new(init),
             debug_name,
         }
+    }
+
+    fn read(&self) -> Rc<T> {
+        let provider = self.provider_tree.get_current();
+        let value = self.provider.get_value(provider.clone());
+
+        println!(
+            "Adding dependent to root: {}",
+            self.provider_tree.call_stack.borrow().last().unwrap()
+        );
+        self.provider.attach_dependent(
+            provider,
+            *self.provider_tree.call_stack.borrow().last().unwrap(),
+        );
+
+        value
+    }
+
+    fn write(&self, value: T) {
+        let provider = self.provider_tree.get_current();
+
+        println!("set root");
+        self.provider.set_value(provider.clone(), value);
+
+        self.provider.notify_dependents(provider.clone());
+    }
+}
+impl<T: 'static> Read<T> for RootNode<T> {
+    fn getp(&self, scope: &'static Scope) -> Rc<T> {
+        {
+            self.provider_tree.scope_stack.borrow_mut().push(scope);
+        }
+
+        let value = self.read();
+
+        {
+            self.provider_tree.scope_stack.borrow_mut().pop();
+        }
+
+        value
+    }
+
+    fn get(&self) -> Rc<T> {
+        self.read()
+    }
+}
+
+impl<T> Write<T> for RootNode<T> {
+    fn set(&self, value: T) {
+        self.write(value);
+    }
+
+    fn setp(&self, value: T, scope: &'static Scope) {
+        {
+            self.provider_tree.scope_stack.borrow_mut().push(scope);
+        }
+
+        self.write(value);
+
+        {
+            self.provider_tree.scope_stack.borrow_mut().pop();
+        }
+    }
+}
+
+impl<T> Debug for RootNode<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.debug_name)
+    }
+}
+
+impl<T: 'static> Accessible<T> for RootNode<T> {}
+
+impl<T> Provided for RootNode<T> {
+    fn get_tree(&self) -> &'static ProviderTree {
+        self.provider_tree
     }
 }
